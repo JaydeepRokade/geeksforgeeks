a065f67945e6321867769ccdb4cf8b8e14fbaec5
JaydeepRokade <150123454+JaydeepRokade@users.noreply.github.com>
2024-09-26 23:37:07 +0530

    Create geeks5.cpp

---
+++ geeks5.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    // Function to find maximum number of consecutive steps
+    // to gain an increase in altitude with each step.
+    int maxStep(vector<int>& arr) {
+        // Your code herefo
+        int ans = 0;
+        int cnt = 0;
+        for(int i = 0;i<arr.size()-1;i++){
+            if(arr[i]<arr[i+1]) cnt ++;
+            else{
+                ans = max(ans,cnt);
+                cnt = 0;
+            }
+        }
+        ans = max(cnt,ans);
+        return ans;
+    }
+};
