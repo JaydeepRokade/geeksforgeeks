f90be67cb8af977cd185bd4d4c5963449f481a32
JaydeepRokade <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-03 17:39:54 +0530

    Create geeks12.cpp

---
+++ geeks12.cpp
@@ -0,0 +1,18 @@
+vector<int> findMajority(vector<int>& nums) {
+     
+        vector<int>ans;
+        unordered_map<int,int>m;
+        for(int i=0;i<nums.size();i++)
+        {
+            m[nums[i]]++;
+        }
+        int x=nums.size()/3;
+        for(auto i:m)
+        {
+            if(i.second>x)
+            ans.push_back(i.first);
+        }
+        sort(ans.begin(),ans.end());
+        if(ans.size()>0) return ans;
+        else return {-1};
+    }
