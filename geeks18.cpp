f9de87af2338c9d1446511e7aba6527a1a058d7f
JaydeepRokade <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-10 23:45:40 +0530

    Update geeks18.cpp

--- geeks18.cpp
+++ geeks18.cpp
@@ -1,11 +1,9 @@
 int maxDistance(vector<int> &arr) {
-        // Code here
         unordered_map<int,int>mp;
         int ans=INT_MIN;
         for(int i=0;i<arr.size();i++)
         {
             if(mp[arr[i]]>0){
-            // {cout<<"T"<<arr[i]<<" "<<mp[arr[i]]<<" ";
                 ans=max(ans,i-mp[arr[i]]+1);
                 continue;
             }
