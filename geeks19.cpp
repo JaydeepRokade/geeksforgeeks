20ce60132605e993940ed2cb709432578fe6fc71
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-11 18:00:18 +0530

    Update geeks19.cpp

--- geeks19.cpp
+++ geeks19.cpp
@@ -1,13 +1,9 @@
 vector<int> rearrange(const vector<int>& arr) {
         vector<int> ans(arr.size(),-1);
-        
         for(int i=0;i<arr.size();i++){
-            
             if(arr[i] != -1){
                 ans[arr[i]] = arr[i];
             }
         }
-        
         return ans;
-        
     }
