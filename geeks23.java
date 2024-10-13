dab523cd8cde38ce4b412c47f181443d48796a35
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-13 18:52:55 +0530

    Create geeks23.java

---
+++ geeks23.java
@@ -0,0 +1,8 @@
+public void deleteAlt(Node head) {
+        // Code Here
+        Node temp = head;
+        while(temp != null && temp.next != null){
+            temp.next = temp.next.next;
+            temp = temp.next;
+        }
+    }
