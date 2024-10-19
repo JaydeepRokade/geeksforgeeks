ea75d2b9a3b90aa429f0705e6cb55e6a5f0a8331
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-19 13:39:19 +0530

    Update geeks27.java

--- geeks27.java
+++ geeks27.java
@@ -1,14 +1,23 @@
 class Solution {
    public:
     string roundToNearest(string str) {
+       
         int n = str.size();
+       
         if (str.back() <= '5') {
+           
             str.back() = '0';
+           
         } else {
+           
             int carry = 10 - (str.back() - '0');
+           
             for (int i = n - 1; carry and i >= 0; i--) {
+               
                 carry += str[i] - '0';
+               
                 str[i] = '0' + (carry % 10);
+               
                 carry /= 10;
             }
             if (carry) str.insert(str.begin(), carry + '0');
