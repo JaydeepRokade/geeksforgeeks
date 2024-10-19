a471c3a128fa7be41e910570ed5611e10624b821
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-19 13:40:11 +0530

    Update geeks27.java

--- geeks27.java
+++ geeks27.java
@@ -7,11 +7,8 @@ class Solution {
         } else {
             int carry = 10 - (str.back() - '0');
             for (int i = n - 1; carry and i >= 0; i--) {
-               
                 carry += str[i] - '0';
-               
                 str[i] = '0' + (carry % 10);
-               
                 carry /= 10;
             }
             if (carry) str.insert(str.begin(), carry + '0');
