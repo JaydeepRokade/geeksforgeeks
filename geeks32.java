06f9c40846d445902918a5148fdf88ace5bb18a4
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-24 23:54:58 +0530

    Update geeks32.java

--- geeks32.java
+++ geeks32.java
@@ -8,10 +8,8 @@ class Solution {
             {
                 arr[i]=arr[i]*2;
                 arr[i+1]=0;
-            }
-            
+            } 
         }
-        
         for(int i=0;i<arr.length;i++)
         {
             if(arr[i]!=0)
@@ -23,7 +21,6 @@ class Solution {
                 cnt++;
             }
         }
-        
         for(int i=0;i<cnt;i++)
         {
             li.add(0);
