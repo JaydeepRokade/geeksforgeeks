e57a5ee4770e66e72bb50ea5a4eba5a00728be13
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-25 15:40:20 +0530

    Create geeks33.java

---
+++ geeks33.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static ArrayList<Integer> alternateSort(int[] arr) {
+        Arrays.sort(arr);
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        int left = 0;                
+        int right = arr.length - 1;  
+
+        while (left <= right) {
+            if (right >= left) {
+                result.add(arr[right--]);  
+            }
+            if (left <= right) {
+                result.add(arr[left++]);   
+            }
+        }
+
+        return result;
+    }
+}
