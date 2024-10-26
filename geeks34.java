10c90e9b2fc8a64a5ee12e0a66eab7e575a5248a
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-26 17:19:40 +0530

    Update geeks34.java

--- geeks34.java
+++ geeks34.java
@@ -1,7 +1,6 @@
 class Solution {
   public:
     int count(struct Node* head, int key) {
-        // add your code here
         int count = 0;
         while(head){
             if(head->data == key )count++;
