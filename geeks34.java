5d550dddde5f15197c9c665eefc9bb058b0403c0
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-26 17:19:21 +0530

    Create geeks34.java

---
+++ geeks34.java
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int count(struct Node* head, int key) {
+        // add your code here
+        int count = 0;
+        while(head){
+            if(head->data == key )count++;
+            head = head->next;
+        }
+        return count;
+    }
+};
