105e22f008094447fef10696e4279bf2a05bec88
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-28 19:33:57 +0530

    Update geeks35.cpp

--- geeks35.cpp
+++ geeks35.cpp
@@ -12,7 +12,6 @@ class Solution {
                  arr[i] = 0;
              }
         }
-      
         for(int i=0;i<n;i++){
             if(arr[i] !=0){
                 ans.push_back(arr[i]);
