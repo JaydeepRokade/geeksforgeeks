f795d5c387f8a702043fb489b73fe85aa05b32f6
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-28 19:33:12 +0530

    Create geeks35.cpp

---
+++ geeks35.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    vector<int> removeDuplicate(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> ans;
+        vector<int>freq(101,-1);
+        for(int i = 0;i<n;i++){
+             if(freq[arr[i]] == -1){
+                 freq[arr[i]] = i;
+             }
+             else {
+                 arr[i] = 0;
+             }
+        }
+       
+        
+        for(int i=0;i<n;i++){
+            if(arr[i] !=0){
+                ans.push_back(arr[i]);
+            }
+        }
+        
+        return ans;
+        
+    }
+};
