683da56a224655c8f828c0f6f5bf90557f23437d
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-30 19:25:12 +0530

    Update geeks37.java

--- geeks37.java
+++ geeks37.java
@@ -1,6 +1,5 @@
 class Solution {
     int countPairsWithDiffK(int[] arr, int k) {
-        // code here
         int count=0;
         Map<Integer,Integer> map=new HashMap<>();
         for(int i:arr)map.put(i,map.getOrDefault(i,0)+1);
