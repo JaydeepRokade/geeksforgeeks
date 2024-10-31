5f7c93a2c8c7843242da165ad76052c1308d418d
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-10-31 20:44:43 +0530

    Create geeks38.cpp

---
+++ geeks38.cpp
@@ -0,0 +1,45 @@
+class Solution {
+  public:
+    Node* sortedInsert(Node* head, int x) {
+        // Code here
+        Node* newNode = new Node();
+        newNode->data = x;
+        if(!head){
+            return newNode;
+        }
+        if(!head->next){
+            if(head->data>x){
+                newNode->next = head;
+                head->prev = newNode;
+                return newNode;
+            }
+            else{
+                newNode->prev = head;
+                head->next = newNode;
+                return head;
+            }
+        }
+        Node* prevNode = head, *currNode = head->next;
+        if(x<head->data){
+            newNode->next = head;
+            head->prev = newNode;
+            return newNode;
+        }
+        while(currNode){
+            if(prevNode->data<=x && currNode->data>=x){
+                newNode->next = currNode;
+                newNode->prev = prevNode;
+                prevNode->next = newNode;
+                currNode->prev = newNode;
+                return head;
+            }
+            else{
+                prevNode = currNode;
+                currNode = currNode->next;
+            }
+        }
+        prevNode->next = newNode;
+        newNode->prev = prevNode;
+        return head;
+    }
+};
