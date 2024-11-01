9d2be263d23af456a16f8b486cd6a332151a2823
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-01 19:30:00 +0530

    Update geeks39.cpp

--- geeks39.cpp
+++ geeks39.cpp
@@ -2,7 +2,6 @@
         int n = arr.size() ;
         long long sum = 0 ;
         for(int i = 0 ; i < n/2 ; i ++){
-         
             sum += (arr[n - i - 1] - arr[i]) ;
         }
         return 2*sum ;
