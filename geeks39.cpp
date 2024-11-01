56e6471d374d09130f93777243c650f84b9bcac5
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-01 19:28:45 +0530

    Create geeks39.cpp

---
+++ geeks39.cpp
@@ -0,0 +1,9 @@
+ sort(arr.begin() , arr.end()) ;
+        
+        int n = arr.size() ;
+        long long sum = 0 ;
+        for(int i = 0 ; i < n/2 ; i ++){
+            sum += (arr[n - i - 1] - arr[i]) ;
+        }
+        
+        return 2*sum ;
