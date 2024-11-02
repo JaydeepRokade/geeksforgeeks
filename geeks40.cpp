45ee0241e6115457a130a86e64d6523f10faf4af
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-02 23:11:51 +0530

    Create geeks40.cpp

---
+++ geeks40.cpp
@@ -0,0 +1,20 @@
+ map<int, int> mp;
+        for(int i=0; i<arr.size(); i++)
+        {
+            if(mp[arr[i]])
+            {
+                if(i - mp[arr[i]] <= k)
+                {
+                    return true;
+                }
+                else
+                {
+                    mp[arr[i]] = i;
+                }
+            }
+            else
+            {
+                mp[arr[i]] = i;
+            }
+        }
+        return false;
