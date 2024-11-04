72b287b3f591677eed18b5a2c3d9c56c89f0e34e
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-04 23:48:21 +0530

    Update geeks42.cpp

--- geeks42.cpp
+++ geeks42.cpp
@@ -7,7 +7,6 @@ class Solution {
             unordered_map<int,unordered_set<int>> mp;
             for(int j=i+1;j<nums.size();j++)
             mp[nums[j]].insert(j);
-            
             for(int j=i+1;j<nums.size();j++)
                 if(mp.find(((-1) * nums[i])-nums[j])!=mp.end())
                     for(auto it : mp[((-1) * nums[i])-nums[j]])
