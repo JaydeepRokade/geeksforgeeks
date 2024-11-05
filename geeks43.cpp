13c1b98de8c387921003a340774b7ac0361c5590
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-05 23:52:47 +0530

    Create geeks43.cpp

---
+++ geeks43.cpp
@@ -0,0 +1,16 @@
+void rotate(vector<vector<int> >& mat) {
+    // Your code goes here
+    int n=mat.size();
+    //step1: take transpose
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            swap(mat[i][j],mat[j][i]);
+        }
+    }
+    //step2//swap first col with last col and 2 col with 2nd last col and so on...
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n/2;j++){
+            swap(mat[i][j],mat[i][n-1-j]);
+        }
+    }
+}
