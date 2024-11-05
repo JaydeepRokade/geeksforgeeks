cab78ef3f01673df50f34a037a07863151bd15dc
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-05 23:53:26 +0530

    Update geeks43.cpp

--- geeks43.cpp
+++ geeks43.cpp
@@ -5,7 +5,6 @@ void rotate(vector<vector<int> >& mat) {
             swap(mat[i][j],mat[j][i]);
         }
     }
-    //step2//swap first col with last col and 2 col with 2nd last col and so on...
     for(int i=0;i<n;i++){
         for(int j=0;j<n/2;j++){
             swap(mat[i][j],mat[i][n-1-j]);
