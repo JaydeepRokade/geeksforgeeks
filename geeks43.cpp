2bd87cbe708e41b1953d22f2239df2229988fac5
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-05 23:53:15 +0530

    Update geeks43.cpp

--- geeks43.cpp
+++ geeks43.cpp
@@ -1,6 +1,5 @@
 void rotate(vector<vector<int> >& mat) {
     int n=mat.size();
-    //step1: take transpose
     for(int i=0;i<n;i++){
         for(int j=i;j<n;j++){
             swap(mat[i][j],mat[j][i]);
