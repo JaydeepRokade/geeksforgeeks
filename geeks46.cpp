9db5ee8bbbd39005249a7255dd8d3ebf1c1ca0fa
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-08 23:42:13 +0530

    Update geeks46.cpp

--- geeks46.cpp
+++ geeks46.cpp
@@ -1,7 +1,6 @@
 class Solution {
   public:
     int minRepeats(string& s1, string& s2) {
-        // code here
          int ans = 1 , n1 = s1.size() ;string s = s1;
         while(n1<s2.size()){
             ans++;
