2b656833d62039a735028080a287b5b4ee0eb73d
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-10 13:27:16 +0530

    Update geeks47.cpp

--- geeks47.cpp
+++ geeks47.cpp
@@ -1,8 +1,7 @@
 class Solution {
   public:
     vector<int> findUnion(vector<int> &a, vector<int> &b) {
-        // Your code here
-        // return vector with correct order of elements
+        // Your code 
          set<int> st(a.begin(),a.end());
         for(auto &i:b) st.insert(i);
         vector<int> ans(st.begin(),st.end());
