8fa613a67412eea60dc630cc80f84988360ea9a5
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-10 13:28:06 +0530

    Update geeks47.cpp

--- geeks47.cpp
+++ geeks47.cpp
@@ -1,7 +1,6 @@
 class Solution {
   public:
     vector<int> findUnion(vector<int> &a, vector<int> &b) {
-        // Your code 
          set<int> st(a.begin(),a.end());
         for(auto &i:b) st.insert(i);
         vector<int> ans(st.begin(),st.end());
