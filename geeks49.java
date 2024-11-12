2faee9074fca20b4ad0f8c7450e3d7b696ff06c4
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-12 10:27:20 +0530

    Update geeks49.java

--- geeks49.java
+++ geeks49.java
@@ -1,5 +1,4 @@
-
-        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+  Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
         for(int i=0; i<arr.length-1;i++){
             if(arr[i][1]>arr[i+1][0]){
                 return false;
