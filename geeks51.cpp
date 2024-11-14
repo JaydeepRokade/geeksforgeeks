ba9b6c2f4524d49d778b23f7b3ef86935324f51d
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-14 12:46:32 +0530

    Create geeks51.cpp

---
+++ geeks51.cpp
@@ -0,0 +1,23 @@
+  
+        int n = arr.size();
+        // min-heap
+        priority_queue<int, vector<int>, greater<int>> pq;
+        
+        for(int i=0; i<=k-1; i++) pq.push(arr[i]);
+        
+        int i=0; int j=k;
+        
+        while(j < n){
+            pq.push(arr[j]);
+            int top = pq.top(); 
+            pq.pop();
+            arr[i] = top;
+            i++;
+            j++;
+        }
+        
+        while(i<n && !pq.empty()){
+            arr[i] = pq.top();
+            pq.pop();
+            i++;
+        }
