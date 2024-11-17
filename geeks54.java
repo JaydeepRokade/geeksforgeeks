4cba95612f5a732f1d02592c47e40de5fbbce5ac
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-17 19:04:16 +0530

    Create geeks54.java

---
+++ geeks54.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static void reverseArray(int arr[]) {
+        int first = 0;
+        int second = arr.length - 1;
+        
+        while (first <= second) {
+            swap(arr, first++, second--);
+        }
+    }
+    
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
