7cba311d7bf5b055098693a526f7b88f931e49f0
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-18 18:56:00 +0530

    Create geeks57.cpp

---
+++ geeks57.cpp
@@ -0,0 +1,33 @@
+
+class Solution {
+  public:
+
+    // Function to rotate an array by d elements in counter-clockwise direction.
+      void rev(vector<int>& arr, int i, int j)
+    {
+        while(i < j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        } 
+    }
+    void rotateArr(vector<int>& arr, int d) 
+    {
+        d = d%(arr.size()); 
+        int i = 0;
+        int j = d - 1;
+        rev(arr,i,j);
+        
+        i = d;
+        j = arr.size() - 1;
+        rev(arr,i,j); 
+        
+        i = 0;
+        j = arr.size() - 1;
+        rev(arr,i,j); 
+    }
+};
+
