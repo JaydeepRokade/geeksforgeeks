9bfb31a83a2a2b3ff1ac2cc442795e8b5e6e2449
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-22 22:06:25 +0530

    Create geeks55.cpp

---
+++ geeks55.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        // code here
+          int n=prices.size();
+         int minPrice=INT_MAX;
+         int maxProfit=0;
+         for(int i=0;i<n;i++)
+         {
+             minPrice=min(minPrice,prices[i]);
+             int profit=prices[i]-minPrice;
+             maxProfit=max(maxProfit,profit);
+             
+         }
+        return maxProfit;
+    }
+};
