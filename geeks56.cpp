529cf6575f2df0564abc0cdb3aa31315ddb9f9f9
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-23 21:17:47 +0530

    Update geeks56.cpp

--- geeks56.cpp
+++ geeks56.cpp
@@ -3,7 +3,6 @@ class Solution {
         Arrays.sort(arr);
         int n = arr.length;
         int res = arr[n-1] - arr[0];
-        
         for(int i =1;i<n;i++){
             int max = Math.max(arr[n-1]-k,arr[i-1]+k);
             int min = Math.min(arr[0]+k, arr[i]-k);
