b3ec6f1ee71233addcbeb70303fafe67a685abfa
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-24 22:04:34 +0530

    Create geeks60.cpp

---
+++ geeks60.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(vector<int> &arr) {
+        // code here...
+         int sum=0;
+        
+        
+        int n=arr.size();
+        int ans=arr[0];
+        for(int i=0;i<n;i++)
+        {
+            sum+=arr[i];
+            if(sum<0)
+            sum=0;
+            ans=max(ans,sum);
+            
+        }
+        sort(arr.begin(),arr.end());
+        if(arr[n-1]<0)
+        return arr[n-1];
+        return ans;
+    }
+};
