f46cbfc9df54248d5c9a8c1bae3ef513d8132598
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2024-11-30 23:44:57 +0530

    Update geeks87.cpp

--- geeks87.cpp
+++ geeks87.cpp
@@ -1,7 +1,5 @@
-
 class Solution {
   public:
-    // Function is to check whether two strings are anagram of each other or not.
     bool areAnagrams(string& s1, string& s2) {
         // Your code here
         if(s1.size()!=s2.size()) return false;
