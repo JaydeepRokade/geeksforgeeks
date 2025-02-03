3e02d9e9cb4d063688c3959c62c13e780f291fa7
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-02-03 23:54:11 +0530

    Create gfg205.cpp

---
+++ gfg205.cpp
@@ -0,0 +1,23 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    int height(Node* node) {
+        if(node==NULL)
+        return -1;
+        int x=height(node->left);
+        int y=height(node->right);
+        return max(x,y)+1;
+    }
+};
