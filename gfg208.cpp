32982a1ab7b93ce4b301a2c7966a786c53ccbfdd
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-02-12 23:57:20 +0530

    Create gfg208.cpp

---
+++ gfg208.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    // Return the Kth smallest element in the given BST
+    void inorderTraversal(Node* root,vector<int> &inorder){
+        if(!root)
+            return;
+        inorderTraversal(root->left,inorder);
+        inorder.push_back(root->data);
+        inorderTraversal(root->right,inorder);
+    }
+    int kthSmallest(Node *root, int k) {
+        vector<int> inorder;
+        inorderTraversal(root,inorder);
+        if(k-1> inorder.size())
+            return -1;
+        return inorder[k-1];
+        
+    }
