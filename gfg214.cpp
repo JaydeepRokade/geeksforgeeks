f4db7c9b4921ffd42e7e0ab5f09e419aff53b9d5
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-02-23 23:57:56 +0530

    Create gfg214.cpp

---
+++ gfg214.cpp
@@ -0,0 +1,23 @@
+  vector<int> nextLargerElement(vector<int>& arr) {
+        // code here
+        stack<int>st;
+        int n = arr.size();
+        st.push(arr.back());
+        arr.back() = -1;
+        for(int i=n-2;i>=0;i--){
+            while(!st.empty() && st.top()<=arr[i]){
+                st.pop();
+            }
+            if(!st.empty() && st.top()>arr[i]){
+                int p = arr[i];
+                arr[i] = st.top();
+                st.push(p);
+            }
+            else{
+                st.push(arr[i]);
+                arr[i] = -1;
+            }
+        }
+        return arr;
+    }
+};
