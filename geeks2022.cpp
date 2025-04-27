1ce589f7325ac941d4a337256fe0fd8f8afd4e27
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-04-28 00:01:33 +0530

    Create geeks2022.cpp

---
+++ geeks2022.cpp
@@ -0,0 +1,23 @@
+pair<int,int> helper(Node* root){
+         if(root == NULL) return {0,0};
+         
+         auto left = helper(root->left);
+         auto right = helper(root->right);
+         
+         //sum including the nodes
+         int a = root->data + left.second + right.second;
+         
+         //sum excluding the nodes
+         
+         int b = max(left.first,left.second) + max(right.first, right.second);
+         
+         
+         return {a,b};
+        }
+    
+    int getMaxSum(Node *root) 
+    {
+        auto ans = helper(root);
+        return max(ans.first,ans.second);
+        
+    }
