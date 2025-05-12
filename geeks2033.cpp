42f72a875201b2fb4edc4895ddb2ce8f7b5e8bd4
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-05-12 23:32:38 +0530

    Update geeks2033.cpp

--- geeks2033.cpp
+++ geeks2033.cpp
@@ -3,7 +3,7 @@ private:
     vector<int> meetCnt;
     priority_queue<int, vector<int>, greater<int>> availableRooms;
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> busyRooms;
-//jsaj js sii sci i;;p sopc oio8sc 
+//jsaj js sii sci i;
     void initializeRooms(int n) 
     {
         meetCnt.resize(n, 0);
