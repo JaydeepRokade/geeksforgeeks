e06472f086c42875d64649f7e13d252bf3b99dc4
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-05-13 10:23:29 +0530

    Create geeks2034.cpp

---
+++ geeks2034.cpp
@@ -0,0 +1,15 @@
+ int nCr(int n, int r) {
+        // code here
+        if(r>n) return 0;
+        long long prod=1;
+        // nCr=n!/(r! * (n-r)!)
+        // 7C3=7!/(4! * 3!)  => (7*6*5)/(3*2*1) => 7*(1/1)*6*(1/2)*5*(1/3)
+        
+        for(int i=0;i<r;i++){
+            prod*=(n-i);
+            prod=prod/(i+1);
+        }
+        
+        return (int)prod;
+    }
+
