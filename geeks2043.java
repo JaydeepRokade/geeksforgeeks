844e8de5c8a9c11357783c4120d2b322a8b6aa44
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-05-21 23:18:20 +0530

    Update geeks2043.java

--- geeks2043.java
+++ geeks2043.java
@@ -6,7 +6,7 @@ class Solution {
         }
         return count;
     }
-    //mcjc kk kxk kxkk kxk
+    //mcjc kk xkk kxk
     public int kthSmallest(int m, int n, int k) {
         int low = 1, high = m * n;
         while (low <= high) {
