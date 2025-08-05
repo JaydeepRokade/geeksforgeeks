ad5503ee048bffc3863dc67a47a3f26d2e37a2e8
Jaydeep-Rokade <jaydeeprokade53@gmail.com>
2025-08-05 16:45:29 +0530

    Update geeks5055.cpp

--- geeks5055.cpp
+++ geeks5055.cpp
@@ -3,7 +3,6 @@ class Solution {
     bool isPalinSent(string &s) {
         int n=s.size();
         int i=0,j=n-1;
-        
         while(i<j){
             while(i<j && !isalnum(s[i]))i++;
             while(i<j && !isalnum(s[j]))j--;
