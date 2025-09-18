5d1cd78f5b408d2a622a343bf49b7d309062d019
Jaydeep-Rokade <jaydeeprokade53@gmail.com>
2025-09-18 15:47:58 +0530

    Create geeks1000.cpp

---
+++ geeks1000.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    vector<int> nextGreater(vector<int> &arr) {
+        stack<int> st;
+        vector<int> temp=arr;
+        reverse(temp.begin(),temp.end());
+        for(auto x:temp)st.push(x);
+        vector<int> ans;
+        for(int i=arr.size()-1;i>=0;i--){
+            while(!st.empty() && st.top() <= arr[i] ) st.pop();
+            if(!st.empty() && st.top() > arr[i] ) ans.push_back(st.top());
+            else if(st.empty()) ans.push_back(-1);
+            st.push(arr[i]);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
